5f9ca0ad6ac93d2a8ba91c6ecb14fb4536695c48
GioDots <dodedodo96@gmail.com>
2020-03-04 19:52:50 +0100

    exercise 2 module 2 done

---
+++ module-2/basics-assignment-2-start/src/app/exercise/exercise.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise',
+  templateUrl: './exercise.component.html',
+  styleUrls: ['./exercise.component.css']
+})
+export class ExerciseComponent implements OnInit {
+  username: string = '';
+  constructor() { }
+
+  resetInput(){
+    this.username = '';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
